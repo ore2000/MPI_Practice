11080ee6b7b61025aca71076bc429a86f11a3264
Oreoluwa Babatunde <obabatun@cheyenne5.cheyenne.ucar.edu>
2020-03-23 16:11:42 -0600

    This is my MPI Practice file

---
+++ MPiPractice.f90
@@ -0,0 +1,15 @@
+program addNumbers
+
+! This is a simple program for MPI Practrice
+ implicit none
+
+! Type declarations
+  real :: a,b, result
+
+! Executable statements
+  a = 12.0
+  b = 15.0
+  result = a+b
+  print *, 'This is the total ',result
+
+end program addNumbers
