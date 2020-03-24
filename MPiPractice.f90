abb68857bbb627241aff4a9888d3916a879b4ecf
Oreoluwa Babatunde <obabatun@cheyenne1.cheyenne.ucar.edu>
2020-03-24 15:50:34 -0600

    This is my file

--- MPiPractice.f90
+++ MPiPractice.f90
@@ -1,15 +1,28 @@
-program addNumbers
+! Decalration of variables 
+  implicit none
+  integer :: n,incx,incy
+  double precision :: i,ix,iy
+  double precision :: alpha,x(:),y(:)
 
-! This is a simple program for MPI Practrice
- implicit none
+  program DaxpyProgram
+      call Daxpy(n, alpha, x, incx, y, incy)
+      print *,'This is the result: ',y
+  end Program DaxpyProgram
 
-! Type declarations
-  real :: a,b, result
 
-! Executable statements
-  a = 12.0
-  b = 15.0
-  result = a+b
-  print *, 'This is the total ',result
-
-end program addNumbers
+  SUBROUTINE Daxpy(n, alpha, x, incx, y, incy)
+  
+  if((incx.eq.1) .and. (incry.eq.1)) then
+    do i =1,n
+       y(i) = alpha*x(i) + y(i)
+    enddo
+  else
+    ix = 1
+    iy = 1
+    do i = 1,n
+       ix = 1 + (i-1)*incx
+       iy = 1 + (i-1)*incy
+    enddo 
+  end if
+  return
+  end SUBROUTINE
