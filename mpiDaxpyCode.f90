6212e68900595e6125c5e5981879ebad00474be6
Oreoluwa Babatunde <obabatun@cheyenne5.ib0.cheyenne.ucar.edu>
2020-04-02 17:49:40 -0600

    Push: 4/2/2020 5:49pm

--- mpiDaxpyCode.f90
+++ mpiDaxpyCode.f90
@@ -1,68 +1,82 @@
 Program DaxpyProgram
 
       implicit none
-      real,dimension(:),allocatable :: x,y
-      integer arraySize,i
-      
-   
-interface
-  SUBROUTINE Daxpy(x,y)
+      include 'mpif.h'
+      real,dimension(:,:),allocatable :: x,y
+      integer i,j
+      real :: start,finish
+      integer :: ierror
+      interface
+         SUBROUTINE Daxpy(x,y,ierror)
 
-      real,dimension(:),intent(in) :: x
-      real,dimension(:), intent(out):: y
-  end SUBROUTINE
-end interface
+             real,dimension(:,:),intent(in) :: x
+             real,dimension(:,:), intent(out):: y
+             integer, intent(out):: ierror
+        end SUBROUTINE
+     end interface
 
- allocate(x(10))
- allocate(y(10))
+     call cpu_time(start)
+     allocate(x(3,3))
+     allocate(y(3,3))
 
-do i = 1,10
- x(i) = (10.2*i)
- y(i) = 10.2
-enddo
+     do i =1,3
+        do j = 1,3
+           x(i,j) = (10.2*i)
+           y(i,j) = 10.2
+        enddo
+     enddo
 
- call Daxpy(x, y)
- print *,'This is the result: '
+     call Daxpy(x, y,ierror)
+     print *,'This is the result: '
  
- arraySize = size(y)
- 
- do i = 1,arraySize
- print *, y(i)
- enddo
+     do i =1,3 
+        do j = 1,3
+           print *,'Matrix(',i,',',j,')= ',y(i,j)
+        enddo
+     enddo
 
-deallocate(x(10))
-deallocate(y(10))
+     deallocate(x)
+     deallocate(y)
+
+     call cpu_time(finish)
+     print *, 'Finished in time', (finish - start)
+     call MPI_FINALIZE(ierror)
 end Program DaxpyProgram
 
 
- SUBROUTINE Daxpy(x,y)
+ SUBROUTINE Daxpy(x,y,ierror)
       implicit none
-      real,dimension(:),intent(in) :: x
-      real,dimension(:), intent(out):: y
-
+      include 'mpif.h'
+      real,dimension(:,:),intent(in) :: x
+      real,dimension(:,:), intent(out):: y
+      integer, intent(out) :: ierror
       !local variables
       integer :: n
       real :: alpha,ix,iy
-      integer :: incx,incy,i
- 
-      n = 10
+      integer :: incx,incy,i,j
+      integer :: rank,mpi_size,message_Item,scattered_Data
+      
+
+      n = 3
       alpha = 4.0
       incx = 1
       incy = 1
-      i=0
+      i = 0  
+      call MPI_INIT(ierror)
+      call MPI_COMM_SIZE(MPI_COMM_WORLD,mpi_size,ierror)
+      call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierror)
+      call MPI_Scatter(y,1,MPI_INT,scattered_Data,1,MPI_INT,0,MPI_COMM_WORLD,ierror)
 
-  if((incx.eq.1) .and.(incy.eq.1)) then
-    do i =1,n
-       y(i) = alpha*x(i) + y(i)
-    enddo
-  else
-    ix = 1
-    iy = 1
-    do i = 1,n
-       ix = 1 + (i-1)*incx
-       iy = 1 + (i-1)*incy
-    enddo
-  end if
-  return
-  end SUBROUTINE
+      if((incx.eq.1) .and.(incy.eq.1)) then
+        do i = 1,n
+           do j =1,n
+              y(i,j) = alpha*x(i,j) + y(i,j)
+           enddo
+       enddo
+       end if
+       return
+       
+end SUBROUTINE
+
+
 
