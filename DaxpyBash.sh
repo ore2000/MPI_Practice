90050f1c9fc4f913068dd4d5c45e52187396a006
Oreoluwa Babatunde <obabatun@cheyenne5.ib0.cheyenne.ucar.edu>
2020-04-21 20:57:07 -0600

    4/21/2020

--- DaxpyBash.sh
+++ DaxpyBash.sh
@@ -1,21 +1,19 @@
 #! /bin/bash -1
 #
 #PBS -A NTDD0002
-#PBS -l walltime=0:00:01
-#PBS -l select=1536:ncpus=36:mpiprocs=50
+#PBS -l walltime=0:05:00
+#PBS -l select=3:ncpus=36:mpiprocs=36
 #PBS -N Daxpy
 #PBS -o checker.out
 #PBS -e errcheck.err
 #PBS -q regular
 
 module purge 
-module load pgi 
 module load openmpi
+module load intel
 
 #Go to project directory
 cd /glade/scratch/obabatun/MPI_Practice
 
-#compile the code 
-mpif90 ProcMpi.f90 -o executable.exe
+mpirun -np 108 ./run.exe
 
-mpirun -np 20 ./executable.exe
--- twoDimensionMPI.f90
+++ twoDimensionMPI.f90
@@ -1,24 +1,23 @@
 Program DaxpyProgram
+
+
       implicit none
-      include 'mpif.h'
       real,dimension(:,:),allocatable :: x,y
       integer i,j,alpha,n
       real :: start,finish
-      integer :: rank,mpi_size,ierror,messageItem,scatter_Data,gather_data
 
       !initializing the variables i,alpha and n.
       i = 0
       alpha = 4.0
-      n = 50000
+      n = 2048
+      
       !allocating a size of n x n memory to matrix x and y 
       allocate(x(n,n))
       allocate(y(n,n))
 
-      call MPI_INIT(ierror)
-      call MPI_COMM_SIZE(MPI_COMM_WORLD,mpi_size,ierror)
-      call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierror)
-      call MPI_Scatter(y,10,MPI_INT,scatter_Data,10,MPI_INT,0,MPI_COMM_WORLD,ierror)
-      
+      !Start timing
+      !call cpu_time(start)
+
       !do loop to initialize the x and y matrix
       do i =1,n
          do j = 1,n
@@ -26,40 +25,29 @@ Program DaxpyProgram
             y(i,j) = 10.2
          enddo
       enddo
-
-     call MPI_GATHER(y,10,MPI_REAL,gather_data,10,MPI_REAL,0,MPI_COMM_WORLD,ierror)
-     !Start timing
-     call cpu_time(start)
-
+     
      !editing refilling the y matrix with scalar multiples of the x matrix
-     do i = 1,n
-        do j =1,n
-           y(i,j) = alpha*x(i,j) + y(i,j)
-        enddo
-     enddo
-
-     !Stop timing.
-     call cpu_time(finish)
-
+     call cpu_time(start)
+         do i = 1,n
+            do j =1,n
+               y(i,j) = alpha*x(i,j) + y(i,j)
+            enddo
+         enddo
+   call cpu_time(finish)
     !print out the results
-    if(rank == 0) then
     print *,'This is the result: '
+
     do i =1,n 
        do j = 1,n
-         if( i .lt. 6 .and. j .lt. 6) then
+       if( i .lt. 6 .and. j .lt. 6) then
           print *,'Matrix(',i,',',j,')= ',y(i,j)
        endif
        enddo
     enddo
-    endif
-
     !deallocate the x and y memory from device
     deallocate(x)
     deallocate(y)
-    
-  
-    if(rank  == 0) then
-       print *,'Finished in time',(finish - start)
-    endif
-    call MPI_FINALIZE(ierror)
+    !stop timing
+    !call cpu_time(finish)
+    print *, 'Finished in time', (finish - start)
 end Program DaxpyProgram
